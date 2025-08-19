3bbf89678ee8817eeb305e5a6e25a5a8cb1bc66c
unknown <akshaykumar72888@gmail.com>
2025-08-19 18:47:30 -0500

    2475. Number of Unequal Triplets in Array

---
+++ python_leetcode/no_of_unique_triplets_in_arr.py
@@ -0,0 +1,44 @@
+# ## Question:
+
+# You are given a 0-indexed array of positive integers nums. Find the number of triplets (i, j, k) that meet the following conditions:
+
+# 0 <= i < j < k < nums.length
+# nums[i], nums[j], and nums[k] are pairwise distinct.
+# In other words, nums[i] != nums[j], nums[i] != nums[k], and nums[j] != nums[k].
+# Return the number of triplets that meet the conditions.
+
+ 
+
+# Example 1:
+
+# Input: nums = [4,4,2,4,3]
+# Output: 3
+# Explanation: The following triplets meet the conditions:
+# - (0, 2, 4) because 4 != 2 != 3
+# - (1, 2, 4) because 4 != 2 != 3
+# - (2, 3, 4) because 2 != 4 != 3
+# Since there are 3 triplets, we return 3.
+# Note that (2, 0, 4) is not a valid triplet because 2 > 0.
+# Example 2:
+
+# Input: nums = [1,1,1,1,1]
+# Output: 0
+# Explanation: No triplets meet the conditions so we return 0.
+ 
+
+
+
+# Code:
+
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+        c=0
+        n=len(nums)
+        for i in range(n):
+            for j in range(i, n):
+                for k in range(j,n):
+                    if 0<=i:
+                        if i<j and j<k and k<n:
+                            if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
+                                c+=1
+        return c
